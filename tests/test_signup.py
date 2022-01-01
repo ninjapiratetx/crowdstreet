bfc59a487a8fd00b6c8461ef4b57c1bfb59a9446
ninjapiratetx <ninjapiratetx@gmail.com>
2021-12-31 21:41:17 -0600

    got first test working

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -7,10 +7,10 @@ TEST_SERVER = "https://test.crowdstreet.com/"
 @pytest.fixture
 def chrome_driver(): 
    driver = webdriver.Chrome()
-   self.driver.get(TEST_SERVER)
+   driver.get(TEST_SERVER)
    yield driver
 
 def test_registration_asked(chrome_driver):
     driver = chrome_driver
-    main_page = page.MainPage(driver)
+    main_page = framework.page.MainPage(driver)
     assert main_page.registration_exists()
